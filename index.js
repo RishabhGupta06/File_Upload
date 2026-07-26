2db4a7df882fa9310d843f75c1209e6cf23e41d2
Rishabh Gupta <rishabhgp06@gmail.com>
2026-07-26 23:54:22 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+
+
+const app = express();
+const port = 8000;
+
+
+app.set('view engine', 'ejs');
+app.set("views",path.resolve("./views"));
+
+app.use(express.json());
+
+
+app.get("/",(req,res) =>{
+    return res.render("homepage");
+})
+
+
+
+app.listen(port,()=> console.log('Server is running'));
