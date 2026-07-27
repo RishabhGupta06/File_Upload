8f863b6620fc3338c8db14c123b15a0318613238
Rishabh Gupta <rishabhgp06@gmail.com>
2026-07-27 23:21:27 +0530

    update:

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const multer = require('multer');
 
+const upload = multer({dest: "uploads/"})
 
 
 const app = express();
@@ -12,14 +14,16 @@ app.set("views",path.resolve("./views"));
 
 
 
-app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 
 app.get("/",(req,res) =>{
     return res.render("home");
 })
 
+app.post("/upload",(req,res) =>{
 
+})
 
 
 app.listen(port,()=> console.log('Server is running'));
